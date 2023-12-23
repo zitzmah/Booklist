1710cb4e5a50cea330ed621ce4c345f1f9ed50fd
zitzmah <haleyzitzmann@gmail.com>
2023-12-23 09:35:31 -0700

    Full Crud

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require("dotenv").config()
 require("./config/db") //bring in our db config
 const express = require("express")
 const morgan = require("morgan") //logger
+const methodOverride = require("method-override")
 
 const app = express();
 const { PORT = 3013 } = process.env;
@@ -18,11 +19,15 @@ const Book = require("./models/Book")
 //************************** */
 app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }))
+//how we get to access the req.body
+app.use(methodOverride("_method"))//lets us use DELETE PUT HTTP verbs
 
 
 //************************** */
 // ROUTES & ROUTER
 //************************** */
+//INDUCES
+
 //Index- GET render all books
 app.get("/books", async (req, res) => {
     //find all of the books
@@ -37,6 +42,47 @@ app.get("/books", async (req, res) => {
 //New- GET for the form to create a new book
 app.get("/books/new", (req, res) => {
     res.render("new.ejs")
+})
+
+//DELETE
+app.delete("/books/:id", async (req, res) => {
+    try {
+        // Find a book and then delete
+        let deletedBook = await Book.findByIdAndDelete(req.params.id)
+        console.log(deletedBook)
+        // redirect back to the index
+        res.redirect("/books")
+
+    } catch (error) {
+        res.status(500).send("something went wrong when deleting")
+    }
+})
+
+
+//UPDATE
+app.put("/books/:id", async (req, res) => {
+    try {
+        //handle our checkbox
+        if (req.body.completed === "on") {
+            req.body.completed = true
+        } else {
+            req.body.completed = false
+        }
+        //Then find by id and update with the req.body
+        //findbyIdAndUpdate - id , data to update, options object
+        let updatedBook = await Book.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true
+            }
+        )
+        //redirect to the show route with the updated book
+        res.redirect(`/books/${updatedBook._id}`)
+    }
+    catch {
+        res.send("something went wrong in this route")
+    }
 })
 
 //Create - POST
@@ -57,6 +103,20 @@ app.post("/books", async (req, res) => {
         res.send(err)
     }
 })
+
+// EDIT
+app.get("/books/edit/:id", async (req, res) => {
+    try {
+        // find the book to edit
+        let foundBook = await Book.findById(req.params.id)
+        res.render("edit.ejs", {
+            book: foundBook
+        })
+    } catch (error) {
+        res.send("hello from the error")
+    }
+})
+
 // Show - GET rendering only one book
 app.get("/books/:id", async (req, res) => {
     // find a book by _id
